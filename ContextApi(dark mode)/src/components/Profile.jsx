6d4dd5915846b1c23ccfc1baf5390b8aabc014cd
arpitkasaudhan <kasaudhanarpit37@gmail.com>
2024-02-06 15:34:00 +0530

    09project theme switcher

--- ContextApi(dark mode)/src/components/Profile.jsx
+++ ContextApi(dark mode)/src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import UserContext from '../context/UserContext'
+
 function Profile() {
     const {user}=useContext(UserContext)
     if(!user) return <div>Please Login</div>
